967daf3996c147737d97503bd0ed4902a85f21fa
Mohamed Wael <100571535+MohamedWaelGD@users.noreply.github.com>
2024-02-10 13:57:59 +0200

    init

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const http = require('http');
+const socketIO = require('socket.io');
+const cors = require('cors');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
+
+app.use(cors());
+
+io.on('connection', (socket) => {
+  console.log('A user connected');
+
+  socket.on('disconnect', () => {
+    console.log('User disconnected');
+  });
+
+  socket.on('joinRoom', (room) => {
+    socket.join(room);
+    console.log(`User joined room: ${room}`);
+  });
+
+  socket.on('draw', (msg) => {
+    socket.to(msg.roomUuid).emit('draw', msg);
+    console.log(`User ${msg.senderId} draw in room: ${msg.roomUuid}`);
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
